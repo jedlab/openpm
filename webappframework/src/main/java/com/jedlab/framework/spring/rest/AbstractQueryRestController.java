dd21509912f0e4c16e71340fae47a81fbcd84db9
omidp <info@jedlab.ir>
2018-03-24 18:43:28 +0430

    protected initial page

--- webappframework/src/main/java/com/jedlab/framework/spring/rest/AbstractQueryRestController.java
+++ webappframework/src/main/java/com/jedlab/framework/spring/rest/AbstractQueryRestController.java
@@ -35,10 +35,10 @@ import com.jedlab.framework.spring.service.JPARestriction;
 public abstract class AbstractQueryRestController<E extends EntityModel>
 {
 
-    private static final int INITIAL_PAGE = 0;
-    private static final int INITIAL_PAGE_SIZE = 5;
-    private static final int BUTTONS_TO_SHOW = 5;
-    private static final int[] PAGE_SIZES = { 5, 10, 20 };
+    protected static final int INITIAL_PAGE = 0;
+    protected static final int INITIAL_PAGE_SIZE = 5;
+    protected static final int BUTTONS_TO_SHOW = 5;
+    protected static final int[] PAGE_SIZES = { 5, 10, 20 };
 
     @Autowired
     protected Validator validator;
